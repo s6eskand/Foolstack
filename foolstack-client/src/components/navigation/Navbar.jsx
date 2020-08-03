76a693ad55854eaacdd5f5daef0a03769507c748
Sam <sam.eskandar00@hotmail.com>
2020-08-03 16:09:00 -0400

    create navbar

--- foolstack-client/src/components/navigation/Navbar.jsx
+++ foolstack-client/src/components/navigation/Navbar.jsx
@@ -7,31 +7,73 @@ import './Navbar.css';
 
 // images
 import logo from '../../media/images/logo-name-resize.png';
+import AuthDialog from "../auth/AuthDialog";
 
-function Navbar() {
-    return(
-        <nav className="nav">
-            <img src={logo} className="logo" alt="foolstack-logo"/>
-            <div className="container">
-                <ul className="nav-links">
-                    <li className="nav-link-1">
-                        <a href="#">Features</a>
-                    </li>
-                    <li className="nav-link-2">
-                        <a href="#">Pricing</a>
-                    </li>
-                    <li className="nav-link-3">
-                        <a href="#">Enterprise</a>
-                    </li>
-                    <hr id="nav-hr-animation"/>
-                </ul>
-            </div>
-            <div className="actions">
-                <button className="login-btn">Login</button>
-                <button className="register-btn">Sign Up</button>
-            </div>
-        </nav>
-    )
+class Navbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            value: 0
+        }
+    }
+
+    handleLoginOpen = () => {
+        this.setState({
+            open: true,
+            value: 0
+        })
+    }
+
+    handleRegisterOpen = () => {
+        this.setState({
+            open: true,
+            value: 1
+        })
+    }
+
+    handleClose = () => {
+        this.setState({open: false})
+    }
+
+    handleTabChange = (e, newValue) => {
+        this.setState({value: newValue})
+    }
+
+    render() {
+        return (
+            <>
+                <AuthDialog
+                    open={this.state.open}
+                    handleTabChange={this.handleTabChange}
+                    handleClose={this.handleClose}
+                    value={this.state.value}
+                />
+                <nav className="nav">
+                    <img src={logo} className="logo" alt="foolstack-logo"/>
+                    <div className="container">
+                        <ul className="nav-links">
+                            <li className="nav-link-1">
+                                <a href="#">Features</a>
+                            </li>
+                            <li className="nav-link-2">
+                                <a href="#">Pricing</a>
+                            </li>
+                            <li className="nav-link-3">
+                                <a href="#">Enterprise</a>
+                            </li>
+                            <hr id="nav-hr-animation"/>
+                        </ul>
+                    </div>
+                    <div className="actions">
+                        <button className="login-btn" onClick={this.handleLoginOpen}>Login</button>
+                        <button className="register-btn" onClick={this.handleRegisterOpen}>Sign Up</button>
+                    </div>
+                </nav>
+            </>
+        )
+    }
 }
 
 export default Navbar;
