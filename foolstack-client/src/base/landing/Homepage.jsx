cb49b52fa884a851a2c6658664628b27053388a7
Sam <sam.eskandar00@hotmail.com>
2020-08-03 16:07:57 -0400

    create landing page

--- foolstack-client/src/base/landing/Homepage.jsx
+++ foolstack-client/src/base/landing/Homepage.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 // components
 import Navbar from "../../components/navigation/Navbar";
+import Header from "../../components/landing/header/Header";
+import About from "../../components/landing/about/About";
 
 function Homepage() {
     return(
         <>
             <Navbar />
+            <Header />
+            <About />
         </>
     )
 }
