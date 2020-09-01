2ca83fa88ab662e75e8f62cad80e4a03ed847703
Sam <sam.eskandar00@hotmail.com>
2020-09-01 18:23:48 -0400

    test cases

---
+++ foolstack-server/src/test/groovy/foolstack/server/project/ServiceSpec.groovy
@@ -0,0 +1,18 @@
+package foolstack.server.project
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ServiceSpec extends Specification implements DomainUnitTest<Service> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
